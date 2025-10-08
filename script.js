13205ec26a3e49d894eda9b2171e8c43826a247d
Mariam <gbadamosimariam49@gmail.com>
2025-10-08 12:21:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var body = document.querySelectorAll("body");
+var cardDisplay = document.querySelector("#cardDisplay");
+var reset = document.querySelector("reser");
+
+var defaultBackground = white;
+var textColor = black;
+
+card1.addEventListener("click", changeColorFunction );
+mainSeletor.style.defaultBackgroundColor = "purple"
